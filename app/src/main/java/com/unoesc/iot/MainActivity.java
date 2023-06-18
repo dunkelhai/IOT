b7de9762097386345a71839fbfc07b2418b83187
Alysson <3alysson@gmail.com>
2023-06-18 04:15:04 -0300

    Funciona

--- app/src/main/java/com/unoesc/iot/MainActivity.java
+++ app/src/main/java/com/unoesc/iot/MainActivity.java
@@ -3,12 +3,24 @@ package com.unoesc.iot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings; import android.webkit.WebView; import android.webkit.WebViewClient; import android.widget.TextView;
+public class MainActivity extends AppCompatActivity { WebView myWebView;
+    TextView servidor;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.wvApp); WebViewClient mWebViewClient = new WebViewClient();
+        myWebView.setWebViewClient(mWebViewClient); WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true); servidor = findViewById(R.id.edtServidor);
+        conectar();
     }
-}
+    public void btnConectar(View view) { conectar();
+    }
+    public void btnLigar(View view) { myWebView.loadUrl(servidor.getText().toString() + "/ledon");
+    }
+    public void btnDesligar(View view) { myWebView.loadUrl(servidor.getText().toString() + "/ledoff");
+    }
+    private void conectar() { myWebView.loadUrl(servidor.getText().toString());
+    } }
